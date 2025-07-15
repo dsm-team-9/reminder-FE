f4ccc09042109dfa3a3c53d7db5f4dd74ab406bb
seeum0507 <kwonstella0507@gmail.com>
2025-07-15 19:00:20 +0900

    feat: change route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
+import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import { Category } from "./components/Category";
-import { Social } from "./components/Social";
-import { Menu } from "./components/Menu";
-import { Topbar } from "./components/Topbar";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="category" element={<Category />} />
-      <Route path="social" element={<Social />} />
-      <Route path="menu" element={<Menu />} />
-      <Route path="topbar" element={<Topbar />} />
-    </Routes>
-  );
+  return <Routes></Routes>;
 }
 
 export default App;
