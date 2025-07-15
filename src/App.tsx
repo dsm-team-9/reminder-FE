1c0c6da01e2c021764251ffa77fee1e649fc9da3
taeyeonlee1 <h2ymb5@gmail.com>
2025-07-15 17:01:06 +0900

    feat: create middle bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Category } from "./components/Category";
 import { Social } from "./components/Social";
+import { Menu } from "./components/menu";
 
 function App() {
   return (
     <Routes>
       <Route path="category" element={<Category />} />
-       <Route path="social" element={<Social />} />
+      <Route path="social" element={<Social />} />
+      <Route path="menu" element={<Menu />} />
     </Routes>
   );
 }
---
+++ src/components/Menu.tsx
@@ -0,0 +1,13 @@
+import "./Menu.css";
+
+export const Menu = () => {
+  return (
+    <>
+      <div className="menu__bar">
+        <div className="menu__container">
+          <div className="menu__title">museum menu</div>
+        </div>
+      </div>
+    </>
+  );
+};
