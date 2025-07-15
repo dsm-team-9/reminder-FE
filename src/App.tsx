77bca94cb1d2c87f7fd4f00c1eeff6ecc45e52d9
seeum0507 <kwonstella0507@gmail.com>
2025-07-15 11:25:52 +0900

    init: initial setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;
