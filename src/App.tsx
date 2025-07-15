0541005a650f283f540ce17d356dbdcfb812865a
taeyeonlee1 <h2ymb5@gmail.com>
2025-07-15 14:50:58 +0900

    feat: create social component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
 import { Category } from "./components/Category";
+import { Social } from "./components/Social";
 
 function App() {
   return (
     <Routes>
-      <Route path="login" element={<Login />} />
       <Route path="category" element={<Category />} />
+       <Route path="social" element={<Social />} />
     </Routes>
   );
 }
