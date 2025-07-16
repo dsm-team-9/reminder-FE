40b994d0dea5e295d45aee22d94b807cd7f71316
seeum0507 <kwonstella0507@gmail.com>
2025-07-16 18:30:26 +0900

    feat: fix category

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Book from "../assets/book.svg";
 import "./Category.css";
 
 export const Category = () => {
-  return (
-    <>
-      <div className="Category-all">
-        <img src={Book} alt="Book illustration" />
+  const [selectedSubject, setSelectedSubject] = useState("math");
 
-        <div className="Category-texts">
-          <div className="Category__title">Category</div>
-          <div className="Categotry__subject">
-            <div className="Category__subject-each">math</div>
-            <div className="Category__subject-each">history</div>
-            <div className="Category__subject-each">science</div>
-            <div className="Category__subject-each">korean</div>
-            <div className="Category__subject-each">society</div>
-          </div>
+  const subjects = ["all", "math", "history", "science", "korean", "society"];
+
+  return (
+    <div className="Category-all">
+      <img src={Book} alt="Book illustration" />
+
+      <div className="Category-texts">
+        <div className="Category__title">Category</div>
+        <div className="Category__subject">
+          {subjects.map((subject) => (
+            <div
+              key={subject}
+              className="Category__subject-each"
+              style={{
+                color: selectedSubject === subject ? "#1C1F42" : "#5F6074",
+              }}
+              onClick={() => setSelectedSubject(subject)}
+            >
+              {subject}
+            </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
