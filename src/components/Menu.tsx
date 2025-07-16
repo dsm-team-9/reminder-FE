1b5832b59a08c792501eff46371cd2ac22c4f750
seeum0507 <kwonstella0507@gmail.com>
2025-07-16 18:30:34 +0900

    feat: fix menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,34 +1,31 @@
-import React, { useState, useEffect } from "react";
+// src/components/Menu.tsx
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import "./Menu.css";
 
-export const Menu = () => {
-  const location = useLocation();
-  const [myNickname, setMyNickname] = useState("내닉네임");
-  const [friendNickname, setFriendNickname] = useState("친구닉네임");
+interface MenuProps {
+  nickname?: string;
+}
 
+export const Menu = ({ nickname = "" }: MenuProps) => {
+  const location = useLocation();
   const [menuTitle, setMenuTitle] = useState("museum menu");
 
   useEffect(() => {
     if (location.pathname === "/") {
-      // Assuming '/' is your homepage
       setMenuTitle("museum menu");
-    } else if (location.pathname === "/friend-museum") {
-      setMenuTitle(`${friendNickname}'s museum`);
-    } else if (location.pathname === "/my") {
-      setMenuTitle(`${myNickname}'s museum`);
+    } else if (location.pathname.includes("/homeDetail")) {
+      setMenuTitle(`${nickname}'s museum`);
     } else {
       setMenuTitle("museum menu");
     }
-  }, [location.pathname, myNickname, friendNickname]);
+  }, [location.pathname, nickname]);
 
   return (
-    <>
-      <div className="menu__bar">
-        <div className="menu__container">
-          <div className="menu__title">{menuTitle}</div>
-        </div>
+    <div className="menu__bar">
+      <div className="menu__container">
+        <div className="menu__title">{menuTitle}</div>
       </div>
-    </>
+    </div>
   );
 };
