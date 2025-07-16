6b0a5153006fb33792c0cc3096c6ea1ceda2333b
seeum0507 <kwonstella0507@gmail.com>
2025-07-16 10:58:32 +0900

    feat: menu variable

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import "./Menu.css";
 
 export const Menu = () => {
+  const location = useLocation();
+  const [myNickname, setMyNickname] = useState("내닉네임");
+  const [friendNickname, setFriendNickname] = useState("친구닉네임");
+
+  const [menuTitle, setMenuTitle] = useState("museum menu");
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      // Assuming '/' is your homepage
+      setMenuTitle("museum menu");
+    } else if (location.pathname === "/friend-museum") {
+      setMenuTitle(`${friendNickname}'s museum`);
+    } else if (location.pathname === "/my-page") {
+      setMenuTitle(`${myNickname}'s museum`);
+    } else {
+      setMenuTitle("museum menu");
+    }
+  }, [location.pathname, myNickname, friendNickname]);
+
   return (
     <>
       <div className="menu__bar">
         <div className="menu__container">
-          <div className="menu__title">museum menu</div>
+          <div className="menu__title">{menuTitle}</div>
         </div>
       </div>
     </>
